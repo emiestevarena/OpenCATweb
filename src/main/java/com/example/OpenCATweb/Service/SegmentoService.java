52ceb3b629cae0c012842ac9dcceeae9369de1cd
emiestevarena <emiliano.estevarena@gmail.com>
2020-11-23 12:19:47 -0300

    segmento service

--- src/main/java/com/example/OpenCATweb/Service/SegmentoService.java
+++ src/main/java/com/example/OpenCATweb/Service/SegmentoService.java
@@ -5,6 +5,13 @@
  */
 package com.example.OpenCATweb.Service;
 
+import com.example.OpenCATweb.Entities.Proyecto;
+import com.example.OpenCATweb.Entities.Segmento;
+import com.example.OpenCATweb.Enums.Languages;
+import com.example.OpenCATweb.Repositories.SegmentoRepository;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,8 +20,79 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SegmentoService {
+    
+    @Autowired
+    private SegmentoRepository segmentoRepository;
     //altasegmento
+    @Transactional
+    public String crearSegmento(String sourceText,Languages source, Languages target,Proyecto proyecto){
+        try{
+            Segmento s = new Segmento();
+            s.setSourceText(sourceText);
+            s.setSource(source);
+            s.setTarget(target);
+            s.setProyecto(proyecto);
+            segmentoRepository.save(s);
+            return "segmento creado";
+        }catch(Exception ex){
+            return ex.getMessage();
+        }
+    }
     //modificarsegmento
+    @Transactional
+    public String modificarSegmento(Segmento s){
+        try{
+            segmentoRepository.save(s);
+            return "segmento guardado";
+        }catch(Exception ex){
+            return ex.getMessage();
+        }
+    }
     //eliminarsegmento
+    @Transactional
+    public String eliminarSegmento(Segmento s){
+        try{
+            segmentoRepository.save(s);
+            return "segmento eliminado";
+        }catch(Exception ex){
+            return ex.getMessage();
+        }
+    }
+    //missegmentos
+    public List<Segmento> misSegmentos(String idProyecto){
+        return segmentoRepository.misSegmentos(idProyecto);
+    }
     //memoriadetraduccion
+    public String memoriaDeTraduccion(String source, String target,Segmento original){
+        List<Segmento> traduccionesDirectas = segmentoRepository.traduccionesDirectas(source, target);
+        List<Segmento> traduccionesInversas = segmentoRepository.traduccionesInversas(source, target);
+        String[] palabrasOriginal = original.getSourceText().split(" ");
+        int totalOriginal = palabrasOriginal.length;
+        int coincidencias = 0;
+        if(traduccionesDirectas!=null && !traduccionesDirectas.isEmpty()){
+        for(Segmento s : traduccionesDirectas){
+            for(String palabra : palabrasOriginal){
+                if(s.getSourceText().contains(palabra)){coincidencias++;}
+            }
+            double porcentaje = coincidencias*100/totalOriginal;
+            if(porcentaje>=70){return s.getTargetText();}
+            else{coincidencias=0;}
+        }
+        }
+        if(traduccionesInversas!=null&&!traduccionesInversas.isEmpty()){
+        for(Segmento s : traduccionesInversas){
+            for(String palabra : palabrasOriginal){
+                if(s.getTargetText().contains(palabra)){coincidencias++;}
+            }
+            double porcentaje = coincidencias*100/totalOriginal;
+            if(porcentaje>=70){return s.getSourceText();}
+            else{coincidencias=0;}
+        }
+        }
+        return "empty";
+    }
+    //getone
+    public Segmento getOne(Long id){
+        return segmentoRepository.getOne(id);
+    }
 }
